f2b7b76586c26cdb37d9cbe48d8dc7cd653dba99
sarah karhan <109114858+Sarah-karhan@users.noreply.github.com>
2023-01-27 18:39:47 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const video = document.getElementById('video')
+
+
+
+//running the models 
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  ]).then(startVideo) //after loading the models start the video
+
+
+
+//to get the web cam to my video element 
+function startVideo() {
+    navigator.getUserMedia(
+        { video: {}}, 
+        //method to get the what comes from the cam (my face ) to be the source of the vid
+        stream=> video.srcObject = stream,
+        err => console.error(err)
+    )
+}
+
+//adding eventListener when video start playing
+video.addEventListener('play', () => {
+
+    //to display the detection in the video
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+    const displaySize = {width: video.width, height: video.height}
+    faceapi.matchDimensions(canvas, displaySize)
+
+    setInterval(async ()=> {
+        //غيري الاسماء
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+        faceapi.draw.drawDetections(canvas, resizedDetections)
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+    
+   
+    }, 100)
+})
